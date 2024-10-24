5e1c115e01e746f9f830f75c9e3eb55e5dcedb66
Raymond <raymond_oleary@hotmail.com>
2024-10-23 19:43:30 -0700

    fixed hamburger menu functons

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerIcon = document.querySelector(".hamburger-icon");
-    const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-    hamburgerIcon.addEventListener("click", function () {
-        hamburgerMenu.classList.toggle("hidden");
-    });
+hamburgerIcon.addEventListener("click", function () {
+    console.log("Hamburger menu clicked!");
+    hamburgerMenu.classList.toggle("hidden");
 });
